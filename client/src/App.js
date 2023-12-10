698c71cf3726f206550d3bffc711ce67bf23c7dc
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-12-10 14:11:52 +0500

    connected server and client sides

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
 
+import { useState } from 'react';
 import './App.css';
-
+import io from 'socket.io-client';
+import Chat from './components/Chat';
+const socket=io.connect("http://localhost:3001");
 function App() {
+  const [username,setUsername]=useState("");
+  const [room,setRoom]=useState("");
+
+  const joinRoom=()=>{
+    if(username!=="" && room!==""){
+      socket.emit("join_room",room );
+    }
+  }
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h3>Join A Chat</h3>
+      <input 
+        className="form-control mb-2 mr-sm-2"
+        type="text" 
+        placeholder="Jane Doe" 
+        onChange={(event)=>setUsername(event.target.value)}
+      />
+      
+      <input 
+        className="form-control mb-2 mr-sm-2"
+        type="text" 
+        placeholder="Room Id"
+        onChange={(event)=>setRoom(event.target.value)}
+      />
+
+      <button onClick={joinRoom}>Join Room</button>
+      <Chat socket={socket} username={username} room={room}/>
     </div>
   );
 }
---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData);
+    }
+  };
+  return (
+    <div>
+      <div className="chat-header"></div>
+      <div className="chat-body"></div>
+      <div className="chat-footer">
+        <input
+          className="form-control mb-2 mr-sm-2"
+          type="text"
+          placeholder="Enter Message"
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
