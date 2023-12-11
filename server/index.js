8dcba7bbab605c85ce5c81a93f2bab626ba5a21c
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-12-12 01:09:50 +0500

    made some changes in the code and styling

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id)
+    console.log("User Disconnected", socket.id);
   });
 });
 
