00d4ccebe8c85ce5fec9f90d536cc0e9236254a9
NimrahMalik <145552320+NimrahMalik@users.noreply.github.com>
2023-12-16 21:29:04 +0500

    added changes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
       {!showChat ? (
         <div className="joinChatContainer">
-          <h3>Join This Chat</h3>
+          <h3>Join A Chat</h3>
           <input
             type="text"
             placeholder="John...."
@@ -36,7 +36,7 @@ function App() {
               setRoom(event.target.value);
             }}
           />
-          <button onClick={joinRoom}>Join This Room</button>
+          <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
